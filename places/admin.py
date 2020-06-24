bc04a8e91fcd867214834e54d6a5410e5b7562d0
Ekbdizzy <ekbdizzy@yandex.ru>
2020-06-24 21:45:42 +0300

    added ImagesPlace preview in admin

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Place, ImagesPlace
 
 
 class ImagesPlaceInline(admin.TabularInline):
     model = ImagesPlace
     extra = 1
+
+    readonly_fields = ('preview',)
+    fields = ('image', 'preview')
+
+    def preview(self, obj):
+        h = 'max-height:200px;'
+        return format_html(f'<img src="{obj.image.url}" width=auto style={h} />')
 
 
 @admin.register(Place)
