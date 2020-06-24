f5138b8166842d14c4c6b403bd74cdbd433ee0a6
Ekbdizzy <ekbdizzy@yandex.ru>
2020-06-24 22:38:26 +0300

    removed useless comments from code

--- places/models.py
+++ places/models.py
@@ -7,7 +7,6 @@ class Place(models.Model):
     title = models.CharField(blank=True, null=True, default='', max_length=200)
     description_short = models.TextField(blank=True, null=True, default='')
     description_long = HTMLField(blank=True, null=True, default='')
-    # description_long = models.TextField(blank=True, null=True, default='')
     coordinates_lng = models.FloatField(blank=True, null=True, default=0)
     coordinates_lat = models.FloatField(blank=True, null=True, default=0)
 
